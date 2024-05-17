37c85c4badf612cf752cc9929f41853f37972192
Arif Mohammed <arifmohammed.technical@gmail.com>
2024-05-17 10:30:32 -0500

    added public REST API

---
+++ src/main/java/com/example/demo/api/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@GetMapping("/hello")
+	public String hello() {
+	    return "hello azure Java Web app!";
+	}	
+}
